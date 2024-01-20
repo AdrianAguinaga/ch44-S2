e1d3b1202613cbc36e257be00b8493361ea0f842
nefersillo <adrian.aguinaga@uabc.edu.mx>
2024-01-20 11:46:03 -0800

    ch44-s3

--- scripts/app.js
+++ scripts/app.js
@@ -1,57 +1,31 @@
-function sayHello(name, lastName) {
-  console.log("Hello " + name + " " + lastName);
+//global variables
+let iconImportant = false;
+function saveTask() {
+  console.log("Saving task");
 }
 
-function sum(num1, num2) {
-  const res = num1 + num2;
-  return res;
-}
-
-function printNumbers() {
-  //print numbers  from 1 to 20
-  //except 7 and 13
-  let sum = 0;
-  let count = 0;
-   
-  let numbers = [
-    12, 4, 123, 4567, 234, 56, 12, 87, 124, 865, 233, 788, 43, 91, 544, 782,
-    653, 845,
-  ];
-  for (let i = 1; i < 21; i++) {
-    if (i != 7 && i != 13) {
-      console.log(i);
-    }
+function changeIcon() {
+  console.log("Changing icon");
+  const nonImportant = "fa-solid fa-umbrella";
+  const isImportant = "fa-solid fa-mug-hot";
+  if (iconImportant) {
+    $("#iImportant").removeClass(isImportant).addClass(nonImportant);
+    iconImportant = false;
+  } else {
+    $("#iImportant").removeClass(nonImportant).addClass(isImportant);
+    iconImportant = true;
   }
-  for (let i = 0; i < numbers.length; i++) {
-    let num = numbers[i];
-    console.log(num);
-    //sum numbers
-    sum = num;
-    //only greater numbers
-    if (num > 500)
-    count += 1;
-  }
-  console.log("the sum of the numbers is: " + sum);
-  console.log("there are "+count+ " number bigger than 500")
+  //please try to restore the icon
 }
-// 1)print every number  in the list
-// 2)print the sum of all numbers
-// 3)print how many numbers are greater than 500
 
 function init() {
-  console.log("Hello world!");
-  const x = "Adrian";
-  sayHello(x, "Aguinaga");
-  sayHello("John", "Doe");
-  const result = sum(21, 12);
-  console.log(result);
-  printNumbers();
+  console.log("This is the parent of everything");
+  //load data
+
+  //hook events
+  $("#btnSave").click(saveTask);
+  $("#iImportant").click(changeIcon);
+  //document.getElementById("btnSave").click(saveTask);
 }
-//create a function that sums two numbers and excecute it in the init
 
 window.onload = init;
-//window.onload = init(); this is a not
-
-//PRINCIPLE
-//DRY -- DONT REPEAT YOUR SELF
-//KISS - KEEP IT SIMPLE
