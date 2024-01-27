893a1c602cac61971cf3aaedec4ee5c41e301ca9
nefersillo <adrian.aguinaga@uabc.edu.mx>
2024-01-27 12:01:44 -0800

    107-s2

---
+++ 107/organika/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Footer from './components/footer';
+import Navbar from './components/navbar';
+import QuantityPicker from './components/quantityPicker';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Navbar></Navbar>
+      <Footer></Footer>
+      <QuantityPicker></QuantityPicker>
+     
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ 107/organika/src/components/quantityPicker.jsx
@@ -0,0 +1,25 @@
+import "./quantityPicker.css";
+import { useState } from "react";
+
+
+function QuantityPicker()
+{
+    function decrease(){
+        console.log("Decreasing");
+    }
+    //create the increase function
+    function increase(){
+        console.log("Increasing");
+    }
+
+
+    return (
+        <div className="quantityPicker">
+        <button onClick={decrease} >-</button>
+        <label>1</label>
+        <button onClick={increase}>+</button>
+        </div>
+    );
+}
+
+export default QuantityPicker;
