32b21ad21f410a7bf8c79fe78c11d5e8e50cfc77
nefersillo <adrian.aguinaga@uabc.edu.mx>
2024-02-01 17:12:31 -0800

    107-S4images

--- 107/organika/src/App.js
+++ 107/organika/src/App.js
@@ -2,15 +2,20 @@
 import './App.css';
 import Footer from './components/footer';
 import Navbar from './components/navbar';
-import QuantityPicker from './components/quantityPicker';
+import Catalog from './pages/catalog';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.min.js"
+
 
 function App() {
   return (
     <div className="App">
       
       <Navbar></Navbar>
+      <Catalog></Catalog>
       <Footer></Footer>
-      <QuantityPicker></QuantityPicker>
+      
+
      
 
 
